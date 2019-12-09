2c6d8e20de881b1f8c07c8a1b1a76576a838fc53
24125423Qaz <sirbu_ruslan@yahoo.com>
2019-12-09 21:54:10 +0200

    First commit

---
+++ application/base/user/apps/collection/posts/language/english/posts_settings_lang.php
@@ -0,0 +1,13 @@
+<?php
+$lang["posts"] = "Posts";
+$lang["character_count"] = "Character Count";
+$lang["character_count_description"] = "If is enabled, the character count will be displayed in the post's composer.";
+$lang["email_errors"] = "Email errors";
+$lang["email_errors_if_enabled"] = "If is enabled you will receive alerts about publishing errors on email";
+$lang["display_groups"] = "Display Groups";
+$lang["display_groups_description"] = "Display groups instead accounts in the posts page";
+$lang["posts_enable_url_field"] = "Enable Url Field";
+$lang["posts_enable_url_field_description"] = "If is enabled you will see the url button in the Posts app.";
+$lang["posts_rss_feeds"] = "RSS Feeds";
+$lang["posts_hashtags_option"] = "Hashtags Suggestion";
+$lang["posts_hashtags_option_description"] = "You will be able to get popular hashtags based on your search.";
